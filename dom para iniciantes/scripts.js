b57f5764eccf98ab4154fc0b3171a3e23c6d7449
alerrandro borges <alerrandrokaton@gmail.com>
2022-08-12 21:49:09 -0300

    atualizando aula dom 1

--- dom para iniciantes/scripts.js
+++ dom para iniciantes/scripts.js
@@ -1,1 +1,9 @@
-window.alert('isso msm');
+// const href = window.location.href
+
+// console.log(href)
+
+// if (href === 'http://127.0.0.1:5500/dom%20para%20iniciantes/o-que-e-dom.html') {
+//     console.log('È igual');
+// }
+
+const h1selecionada = document.querySelector('h1')
