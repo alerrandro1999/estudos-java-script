172dc86405b0f6119f4a232b5a61883fe388c264
alerrandro borges <alerrandrokaton@gmail.com>
2022-08-12 22:07:52 -0300

    atualizando DOM para iniciantes

--- dom para iniciantes/scripts.js
+++ dom para iniciantes/scripts.js
@@ -1,9 +1,17 @@
-// const href = window.location.href
+const href = window.location.href
 
-// console.log(href)
+console.log(href)
 
-// if (href === 'http://127.0.0.1:5500/dom%20para%20iniciantes/o-que-e-dom.html') {
-//     console.log('È igual');
-// }
 
 const h1selecionada = document.querySelector('h1')
+
+const h1classes = h1selecionada.classList
+
+h1selecionada.addEventListener('click', function () {
+    console.log('clicou em ', h1classes);
+})
+
+window.navigator.language
+
+window.innerWidth
+
