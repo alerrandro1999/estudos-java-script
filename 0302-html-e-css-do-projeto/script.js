78d2ab288fe4ec98d04452027b0f715563e00f55
alerrandro borges <alerrandrokaton@gmail.com>
2022-08-12 22:24:40 -0300

    seleção de elementos 01

---
+++ 0302-html-e-css-do-projeto/script.js
@@ -0,0 +1,12 @@
+const animais = document.getElementById('animais');
+
+const gridsection = document.getElementsByClassName('grid-section')
+
+const primeirali = document.querySelector('li')
+
+const primeiraul = document.querySelector('ul')
+
+const linkinternos = document.querySelector('[href^="#"]')
+
+const animaisimg = document.querySelectorAll('.animais img')
+
